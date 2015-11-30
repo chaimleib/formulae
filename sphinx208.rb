616f17182099207310c8b6b7a0d5342215b78733
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-11-30 13:57:07 -0800

    more cleanup with brew audit --strict

--- sphinx208.rb
+++ sphinx208.rb
@@ -1,15 +1,17 @@
-require "formula"
-
 class Sphinx208 < Formula
+  desc "Full-text search engine server"
   homepage "http://www.sphinxsearch.com"
   url "http://sphinxsearch.com/files/sphinx-2.0.8-release.tar.gz"
   sha256 "bd699bf34635558e4d9d4d8bdcf5fcdd9c2e8f6c614824bec793b0010fa6cda3"
 
-  option "mysql", "Force compiling against MySQL"
-  option "pgsql", "Force compiling against PostgreSQL"
+  option "with-mysql", "Force compiling against MySQL"
+  option "with-pgsql", "Force compiling against PostgreSQL"
 
-  depends_on :mysql if build.include? "mysql"
-  depends_on :postgresql if build.include? "pgsql"
+  deprecated_option "mysql" => "with-mysql"
+  deprecated_option "pgsql" => "with-postgresql"
+
+  depends_on :mysql if build.with? "mysql"
+  depends_on :postgresql if build.with? "postgresql"
 
   conflicts_with "sphinx"
 
@@ -18,13 +20,8 @@ class Sphinx208 < Formula
               --disable-dependency-tracking
               --localstatedir=#{var}]
 
-    %w[mysql pgsql].each do |db|
-      if build.include? db
-        args << "--with-#{db}"
-      else
-        args << "--without-#{db}"
-      end
-    end
+    args << "--{#{build.with? "mysql" ? "with" : "without"}-mysql"
+    args << "--{#{build.with? "postgresql" ? "with" : "without"}-pgsql"
 
     system "./configure", *args
     system "make install"
