19b0126d7514b9820b06edf5cb705cff4279be47
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-12-17 12:22:01 -0800

    upd: libxml280.rb using brew audit --strict

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ brew tap chaimleib/formulae
 ```
 
 ## Formulae provided
-* sphinx208 - The latest versions of [sphinx](http://sphinxsearch.com/) are not backwards-compatible, so this formula provides an older version. Modified from https://github.com/shosti/homebrew-versions/blob/b7c09538a91a7dc13937be26a94067de5e05711f/sphinx208.rb
-* libxml280 - The 2.9.x series of libxml2 is incompatible with older versions of the Ruby gems `libxml-ruby` and `xmlsec-ruby`. Taken from https://raw.githubusercontent.com/Homebrew/homebrew/763d94665ca439653a27ea03da87af96ba66f5b4/Library/Formula/libxml2.rb
+* sphinx208 - The latest versions of [sphinx](http://sphinxsearch.com/) are not backwards-compatible, so this formula provides an older version. Modified from [https://github.com/shosti/homebrew-versions/blob/b7c09538a91a7dc13937be26a94067de5e05711f/sphinx208.rb]()
+* libxml280 - The 2.9.x series of libxml2 is incompatible with older versions of the Ruby gems `libxml-ruby` and `xmlsec-ruby`. Modified from [https://github.com/Homebrew/homebrew/blob/763d94665ca439653a27ea03da87af96ba66f5b4/Library/Formula/libxml2.rb]()
 
--- libxml280.rb
+++ libxml280.rb
@@ -1,9 +1,8 @@
-require 'formula'
-
-class Libxml2 < Formula
-  homepage 'http://xmlsoft.org'
-  url 'ftp://xmlsoft.org/libxml2/libxml2-2.8.0.tar.gz'
-  sha256 'f2e2d0e322685193d1affec83b21dc05d599e17a7306d7b90de95bb5b9ac622a'
+class Libxml280 < Formula
+  desc "GNOME XML library"
+  homepage "http://xmlsoft.org"
+  url "ftp://xmlsoft.org/libxml2/libxml2-2.8.0.tar.gz"
+  sha256 "f2e2d0e322685193d1affec83b21dc05d599e17a7306d7b90de95bb5b9ac622a"
 
   keg_only :provided_by_osx
 
@@ -13,7 +12,7 @@ class Libxml2 < Formula
   end
 
   option :universal
-  option 'with-python', 'Compile the libxml2 Python 2.x modules'
+  option "with-python", "Compile the libxml2 Python 2.x modules"
 
   def install
     ENV.universal_binary if build.universal?
@@ -23,25 +22,25 @@ class Libxml2 < Formula
                           "--without-python"
     system "make"
     ENV.deparallelize
-    system "make install"
+    system "make", "install"
 
-    if build.include? 'with-python'
+    if build.with? "python"
       # Build Python bindings manually
-      cd 'python' do
-        python_lib = lib/which_python/'site-packages'
-        ENV.append 'PYTHONPATH', python_lib
+      cd "python" do
+        python_lib = lib/which_python/"site-packages"
+        ENV.append "PYTHONPATH", python_lib
         python_lib.mkpath
 
         archs = archs_for_command("python")
         archs.remove_ppc!
         arch_flags = archs.as_arch_flags
 
-        ENV.append 'CFLAGS', arch_flags
-        ENV.append 'LDFLAGS', arch_flags
+        ENV.append "CFLAGS", arch_flags
+        ENV.append "LDFLAGS", arch_flags
 
         unless MacOS::CLT.installed?
           # We can hijack /opt/include to insert SDKROOT/usr/include
-          inreplace 'setup.py', '"/opt/include",', "'#{MacOS.sdk_path}/usr/include',"
+          inreplace "setup.py", "'/opt/include',", "'#{MacOS.sdk_path}/usr/include',"
         end
 
         system "python", "setup.py",
@@ -52,6 +51,10 @@ class Libxml2 < Formula
   end
 
   def which_python
-    "python" + `python -c 'import sys;print(sys.version[:3])'`.strip
+    "python" + `python -c "import sys;print(sys.version[:3])"`.strip
+  end
+
+  test do
+    assert_match /#{version}/, shell_output("#{bin}/xml2-config --version")
   end
 end
