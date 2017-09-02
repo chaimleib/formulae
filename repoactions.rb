9a17b74dcfc059a87ca5fdf390885777e9082e9a
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2017-09-01 18:03:46 -0700

    fix repoactions sha

--- repoactions.rb
+++ repoactions.rb
@@ -2,7 +2,7 @@ class Repoactions < Formula
   desc "Run a script whenever you enter a git repo"
   homepage "https://github.com/chaimleib/repoactions"
   url "https://github.com/chaimleib/repoactions/archive/v0.0.10.tar.gz"
-  sha256 "d3de6b296cd8981ed627dc058885736ea2e4b962eb9716c9408ea2d2f94445b6"
+  sha256 "db389935887288ca5d57c75f0b705f73702f86fdc97d0d79f60fe1769e9c3443"
   option "with-rc=", "Specify where to inject login setup (default: ~/.profile)"
 
   def install
